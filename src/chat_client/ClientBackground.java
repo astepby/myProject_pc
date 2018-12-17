373050c3c8757e5d9e0a89b5cf0d06ca9a58a523
astepby <astepby@users.noreply.github.com>
2018-12-17 21:33:55 +0900

    ch07. 08:25

--- src/chat_client/ClientBackground.java
+++ src/chat_client/ClientBackground.java
@@ -1,14 +1,28 @@
 package chat_client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+
+
 public class ClientBackground {
 	Socket socket;
+	
+	DataInputStream in;
+	DataOutputStream out;
+	
 	public void connect() {
 		try {
 			socket = new Socket("127.0.0.1", 7777);
 			System.out.println("Connected to Server.");
+			
+			out = new DataOutputStream(socket.getOutputStream());
+			in = new DataInputStream(socket.getInputStream());
+
+			out.writeUTF("This is client.");
+			System.out.println("msg sent.");
 		} catch (IOException e) {
 			// TODO: handle exception
 		}
--- src/chat_server/ServerBackground.java
+++ src/chat_server/ServerBackground.java
@@ -1,5 +1,7 @@
 package chat_server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,12 +10,21 @@ public class ServerBackground {
 	ServerSocket serverSocket;
 	Socket socket;
 	
+	DataInputStream in;
+	DataOutputStream out;
+	
 	public void setting() {
 		try {
 			serverSocket = new ServerSocket(7777);
 			System.out.println("대기중...");
 			socket = serverSocket.accept();
 			System.out.println(socket.getInetAddress()+" connected.");
+			
+			out = new DataOutputStream(socket.getOutputStream());
+			in = new DataInputStream(socket.getInputStream());
+			
+			String msg = in.readUTF();
+			System.out.println("[Client]"+msg);
 			
 			
 		} catch (IOException e) {
