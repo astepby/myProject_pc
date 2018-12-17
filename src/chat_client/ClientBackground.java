ff665259fbbc944209d260868f48822ca2186e33
astepby <astepby@users.noreply.github.com>
2018-12-17 20:35:16 +0900

    ClientBackground

---
+++ src/chat_client/ClientBackground.java
@@ -0,0 +1,19 @@
+package chat_client;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientBackground {
+	Socket socket;
+	public void connect() {
+		try {
+			socket = new Socket("127.0.0.1", 7777);
+			System.out.println("Connected to Server.");
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+	}
+}
